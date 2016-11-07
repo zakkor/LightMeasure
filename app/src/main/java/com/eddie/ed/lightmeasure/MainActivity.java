54ce63ef8b32738e0ea8ac8c05dd048a43f5a11f
Edward <ed@localhost.localdomain>
2016-11-07 18:40:39 +0200

    Add selectable threshold, progress bar that measures seconds

--- app/src/main/java/com/eddie/ed/lightmeasure/MainActivity.java
+++ app/src/main/java/com/eddie/ed/lightmeasure/MainActivity.java
@@ -8,6 +8,12 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Chronometer;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.Vector;
@@ -25,14 +31,30 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private Runnable runnable;
     private int delay = 1000; //1 second
 
+    private Handler barhandler = new Handler();
+    private Runnable barrunnable;
+    private int bardelay = 100; //1 second
+
     private float currentReading = 0;
     private float lastReading = 0;
+
+    private ProgressBar bar;
+
+    private EditText editText;
+
+    private int threshold = 150;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        editText = (EditText)findViewById(R.id.editText);
+        if (editText != null) {
+            editText.setText("150");
+        }
 
         current = (TextView)findViewById(R.id.current);
         if (current != null) {
@@ -44,8 +66,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             average.setText("Merge boss");
         }
 
+        bar = (ProgressBar)findViewById(R.id.bar);
+
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
 
         handler.postDelayed(new Runnable() {
             public void run() {
@@ -55,15 +80,43 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
                 runnable=this;
 
+                bar.setProgress(0);
+
                 handler.postDelayed(runnable, delay);
             }
         }, delay);
+
+        barhandler.postDelayed(new Runnable() {
+            public void run() {
+                barrunnable=this;
+
+                bar.setProgress(bar.getProgress() + 10);
+
+                barhandler.postDelayed(barrunnable, bardelay);
+            }
+        }, bardelay);
+
+        editText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if (keyEvent.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+                    threshold = Integer.valueOf(editText.getText().toString());
+
+                    if(getCurrentFocus()!=null) {
+                        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                        inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                    }
+                }
+                return false;
+            }
+        });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         mSensorManager.registerListener(this, mLight, SensorManager.SENSOR_DELAY_GAME);
+
     }
 
     @Override
@@ -83,7 +136,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
             currentReading = event.values[0];
 
-            if (currentReading - lastReading >= 18) {
+            if (currentReading - lastReading >= threshold) {
                 flashes++;
             }
 
